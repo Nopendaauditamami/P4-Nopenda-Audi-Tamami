e0e9bf83f4fb394c69421c42e6bded86b91061f4
Nopendaauditamami <64614667+Nopendaauditamami@users.noreply.github.com>
2020-05-13 07:42:02 -0700

    Create Jobsheet 2 C++.cpp
    mengisi dan menampilkan elemen array(dinamis)

---
+++ Jobsheet 2 C++.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main ()
+{
+	cout <<"Nama : Nopenda Audi tamami/nNIM : F1B019107/nKelompok : 23/n/n/n";
+	int nim[7]={7,2,6,3,5,8,9};
+	cout<<"Nim ke-1 adalah"<<nim[0]<<endl;
+	cout<<"Nim ke-2 adalah"<<nim[1]<<endl;
+	cout<<"Nim ke-3 adalah"<<nim[2]<<endl;
+	cout<<"Nim ke-4 adalah"<<nim[3]<<endl;
+	cout<<"Nim ke-5 adalah"<<nim[4]<<endl;
+	cout<<"Nim ke-6 adalah"<<nim[5]<<endl;
+	cout<<"Nim ke-7 adalah"<<nim[6]<<endl;
+}
