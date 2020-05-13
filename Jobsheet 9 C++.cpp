206a6a49e0934230224240bb4ce59fa17341de09
Nopendaauditamami <64614667+Nopendaauditamami@users.noreply.github.com>
2020-05-13 07:58:46 -0700

    Create Jobsheet 9 C++.cpp
    Array sebagai karakter

---
+++ Jobsheet 9 C++.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int i;
+	char nama [17]={'N','O','P','N','N','A'};
+	char nama2[17]="NOPENDA";
+	cout<<"array per karakter=";
+	for(i=0;i<5;i++)
+	{
+		cout<<nama[i]<<",";
+	}
+	cout<<endl;
+	cout<<"array string="<<nama2<<endl<<endl;
+}
